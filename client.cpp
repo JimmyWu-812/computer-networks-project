adb016abf123c519552c1e72596aadb3f818bfec
Jimmy <jimmy881122naruto@gmail.com>
2021-11-24 19:36:04 +0800

    client.cpp

--- client.cpp
+++ client.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
+#include <fstream>
 #include <arpa/inet.h>
 
 #define DOMAIN AF_INET
@@ -14,17 +15,33 @@
 using namespace std;
 
 int main(int argc, char** argv){
-    char buffer[BUF_SIZE];
-    int client_fd = socket(DOMAIN, TYPE, PROTOCOL);
-    int port = atoi(argv[2]);
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = DOMAIN;
-    inet_pton(DOMAIN, argv[1], &addr.sin_addr);
-    addr.sin_port = htons(port);
-    connect(client_fd, (struct sockaddr*)&addr, sizeof(addr));
-    while(cin.getline(buffer,sizeof(buffer))){
-        send(client_fd, buffer, sizeof(buffer), 0);
+    char buffer[BUF_SIZE], ch;
+    const char* delimiter = ":";
+    char *ip_address = strtok((char *)argv[1], delimiter);
+    int port = atoi(strtok(NULL, delimiter));
+    cout << port << " " << ip_address << endl;
+    ifstream file;
+    string file_name;
+    int client_socket = socket(DOMAIN, TYPE, PROTOCOL);
+    int addr_length, i = 0;
+    struct sockaddr_in address;
+    // memset(&addr, 0, addr_length);
+    address.sin_family = DOMAIN;
+    address.sin_addr.s_addr = inet_addr(ip_address);
+    address.sin_port = htons(port);
+    addr_length = sizeof(address);
+    connect(client_socket, (struct sockaddr*)&address, addr_length);
+    while(true){
+        cin >> file_name;
+        file.open(file_name);
+        while(file.get(ch) && i<BUF_SIZE) {
+	    	buffer[i] = ch;
+	    	i++;
+	    }
+	    // cout << buffer << endl;
+        send(client_socket, buffer, BUF_SIZE, 0);
+        file.close();
     }
-    close(client_fd);
+    close(client_socket);
+    return 0;
 }
